e54e8f41ea9aa179fe6f8e0f77d63e21941fa1eb
Lizaveta <st146606@student.spbu.ru>
2025-10-30 06:16:15 +0000

    Add my solution

---
+++ metal_rod.py
@@ -0,0 +1,24 @@
+def metal_rod(prices, n):
+    best_value = [0] * (n + 1)     
+    best_pieces = []                    
+    for i in range(n + 1):
+        best_pieces.append([])
+
+    for r in range(1, n + 1):
+        for c in range(1, r + 1):
+            if c <= len(prices):
+                current_value = prices[c - 1] + best_value[r - c]
+                if current_value > best_value[r]:
+                    best_value[r] = current_value
+                    best_pieces[r] = [c] + best_pieces[r - c]
+
+    return best_pieces[n], best_value[n]
+
+
+prices = [1, 5, 8, 9, 10, 17, 17, 20]
+n = 8
+
+pieces, money = metal_rod(prices, n)
+
+print("list of lengths:", pieces)
+print("total money gained:", money)
